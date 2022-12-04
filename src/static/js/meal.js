d4bba2f0046f5eee95a21617f0955c7d695d1a3d
Tânia Guedes <93449169+TaniaGuedes5@users.noreply.github.com>
2022-12-04 22:38:55 +0000

    Update meal.js

--- src/static/js/meal.js
+++ src/static/js/meal.js
@@ -1,7 +1,7 @@
 $(function() {
     var Accordion = function(el, multiple) {
       this.el = el || {};
-      // more then one submenu open?
+    //  more then one submenu open?
       this.multiple = multiple || false;
       
       var dropdownlink = this.el.find('.dropdownlink');
@@ -22,6 +22,36 @@ $(function() {
       if(!e.data.multiple) {
         //show only one menu at the same time
         $el.find('.submenuItems').not($next).slideUp().parent().removeClass('open');
+      }
+    }
+    
+    var accordion = new Accordion($('.accordion-menu'), false);
+  })
+
+  $(function() {
+    var Inside = function(el, multiple) {
+      this.el = el || {};
+      // more then one submenu open?
+      this.multiple = multiple || false;
+      
+      var insideDropdown = this.el.find('.insideDropdown');
+      insideDropdown.on('click',
+                      { el: this.el, multiple: this.multiple },
+                      this.dropdown);
+    };
+    
+    Inside.prototype.dropdown = function(e) {
+      var $el = e.data.el,
+          $this = $(this),
+          //this is the ul.submenuItems
+          $next = $this.next();
+      
+      $next.slideToggle();
+      $this.parent().toggleClass('open');
+      
+      if(!e.data.multiple) {
+        //show only one menu at the same time
+        $el.find('.kkk').not($next).slideUp().parent().removeClass('open');
       }
     }
     
